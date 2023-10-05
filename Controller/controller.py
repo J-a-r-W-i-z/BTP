eb36fad8091aca44cf0d6a1a3305375c30288576
anuj <anujkakde24@gmail.com>
2023-10-05 22:02:01 +0530

    Skeleton Complete

--- Controller/controller.py
+++ Controller/controller.py
@@ -1,11 +1,72 @@
 import socket
 import threading
-import cv2
-import numpy as np
+import paramiko
+import os
+import re
+import time
+
+# Constants
+RASPBERRY_PIS = [
+    {"ip": "192.168.53.149", "username": "pi1", "password": "raspberry1"},
+    {"ip": "192.168.53.183", "username": "pi1", "password": "raspberry1"},
+    # Add more Raspberry Pis as needed
+]
 
 # Define server settings
 HOST = '0.0.0.0'  # Listen on all available network interfaces
-PORT = 8080      # Choose a port for your server
+PORT = 8084      # Choose a port for your server
+
+# Data structure to store memory usage
+memory_usage = {}
+for i in range(len(RASPBERRY_PIS)):
+    memory_usage[i]=0
+
+# Create a lock
+mutex = threading.Lock()
+
+# Function to SSH into the Raspberry Pi and transfer files
+def ssh_work(image_path, output_path):
+
+    print("function called")
+    # Find Raspberry Pi with maximum free memory
+    mutex.acquire()
+    min_memory_pi = max(memory_usage, key=lambda k: memory_usage[k])
+    mutex.release()
+
+    min_memory_pi =0
+    # Connect to the Raspberry Pi
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    ssh_client.connect(
+        RASPBERRY_PIS[min_memory_pi]["ip"],
+        username=RASPBERRY_PIS[min_memory_pi]["username"],
+        password=RASPBERRY_PIS[min_memory_pi]["password"],
+    )
+    print(f'Connected to Raspi No. : {min_memory_pi}')
+
+    # Transfer the input image to the Raspberry Pi
+    sftp = ssh_client.open_sftp()
+    sftp.chdir("Service")
+    sftp.mkdir(image_path)
+    sftp.put(image_path+".jpg", f"./{image_path}/input_image.jpg")
+    sftp.close()
+
+    print("here")
+    # Run docker image
+    stdin, stdout, stderr = ssh_client.exec_command(f'docker run -v ~/Service:/app image-bw-converter {image_path}')
+    exit_status = stdout.channel.recv_exit_status() 
+
+    print("done")
+    # Transfer the output image from Raspberry Pi
+    sftp = ssh_client.open_sftp()
+    sftp.get(f"Service/{image_path}/output_image.jpg", output_path)
+    sftp.close()
+
+    stdin, stdout, stderr = ssh_client.exec_command(f'cd Service; rm -r {image_path}')    
+
+    ssh_client.close()
+    return True
 
 # Function to handle client connections
 def handle_client(client_socket):
@@ -28,20 +89,84 @@ def handle_client(client_socket):
     if image_data_received:
         # Remove the custom termination sequence from the image data
         image_data = image_data.replace(b"IMAGE_END", b"")
+        
+        # Set path for input and output image
+        host, port = client_socket.getpeername()
+        input_image_path = "client_"+str(host)+"_"+str(port)
+        output_image_path = "client_"+str(host)+"_"+str(port)+ "_output_image.jpg"
+        
+        with open(input_image_path+".jpg", "wb") as f:
+            f.write(image_data)
+        f.close()
+        
+        # Transfer the control to the Raspberry Pi
+        if ssh_work(input_image_path, output_image_path):
+            print("Raspberry Pi work complete")
 
-        # Convert the received image to black and white
-        image_np = np.frombuffer(image_data, dtype=np.uint8)
-        image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-        if image is not None:
-            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            _, img_encoded = cv2.imencode(".png", gray_image)
-            client_socket.sendall(img_encoded.tobytes())
-        else:
-            client_socket.sendall(b"Error processing image")
+        with open(output_image_path, "rb") as f:
+            data = f.read()
+        
+        # Send file back to the client
+        client_socket.sendall(data)
+
     else:
         client_socket.sendall(b"Error: Image data not received properly")
 
     client_socket.close()  # Close the client socket when done
+    os.remove(input_image_path+".jpg")
+    os.remove(output_image_path)
+    print("Socket Closed")
+
+############################################################################################################
+#                            Thread to periodically update memory usage data                               #
+############################################################################################################
+
+# Thread to periodically update memory usage data
+def update_memory_usage():
+    while True:
+        print("Getting memory usage...")
+        for i in range(len(RASPBERRY_PIS)):
+            # Update memory_usage dictionary
+            temp = get_memory_usage(i)
+            mutex.acquire()
+            memory_usage[i] = temp
+            mutex.release()
+        time.sleep(30)  # Update every 30 seconds
+
+# Replace this function with actual memory usage retrieval logic
+def get_memory_usage(pi_idx):
+    # Return memory usage as a float (e.g., 0.75 for 75% usage)
+    # Connect to the Raspberry Pi
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    ssh_client.connect(
+        RASPBERRY_PIS[pi_idx]["ip"],
+        username=RASPBERRY_PIS[pi_idx]["username"],
+        password=RASPBERRY_PIS[pi_idx]["password"],
+    )
+    # Execute the command to get RAM utilization (using 'free' command)
+    stdin, stdout, stderr = ssh_client.exec_command('free -m | grep "Mem:"')
+
+    # Parse the RAM utilization value from the command output
+    output = stdout.read().decode('utf-8')
+    print(output)
+    memory_info = re.findall(r'\d+', output)
+    # The "free" memory value is the third value in the list
+    free_memory = int(memory_info[2])
+    print(free_memory)
+    ssh_client.close()
+
+    return free_memory
+
+# Start memory usage update thread
+memory_thread = threading.Thread(target=update_memory_usage)
+memory_thread.daemon = True
+memory_thread.start()
+
+############################################################################################################
+#                                                 Ends                                                     #
+############################################################################################################
 
 # Create the server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- Service/convert.py
+++ Service/convert.py
@@ -1,12 +1,13 @@
 import cv2
+import sys
 
 def convert_to_bw(input_path, output_path):
     # Read the input image
     image = cv2.imread(input_path)
 
-    if image is None:
-        print("Error: Unable to read the image.")
-        return
+#    if image is None:
+ #       print("Error: Unable to read the image.")
+  #      return
 
     # Convert the image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -15,7 +16,7 @@ def convert_to_bw(input_path, output_path):
     cv2.imwrite(output_path, gray_image)
 
 if __name__ == "__main__":
-    input_path = "input_image.jpg"  # Replace with the path to your input image
-    output_path = "output_image.jpg"  # Replace with the desired output path
+    input_path = "./" + str(sys.argv[1]) + "/input_image.jpg"  # Replace with the path to your input image
+    output_path = "./" + str(sys.argv[1]) + "/output_image.jpg"  # Replace with the desired output path
     convert_to_bw(input_path, output_path)
-    print(f"Image converted to black and white and saved as {output_path}")
+    print("Converted")
